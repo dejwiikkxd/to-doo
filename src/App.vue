ec4c152284c8b19d49ad4ccd44eecd50c0e9a675
dejwiikkxd <dejwkrevnak@outlook.com>
2024-02-28 18:44:14 +0100

    popis

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import {ref, onMounted, computed, watch} from 'vue'
 const todos = ref([])
 const name = ref('')
 
+const input_title = ref('')
 const input_content = ref('')
 const input_priority = ref(null)
 
@@ -12,20 +13,23 @@ const todos_asc = computed(() => todos.value.sort((a,b) => {
 }))
 
 const addTodo = () => {
-  if (input_content.value.trim() === '' || input_priority.value === null){
+  if (input_title.value.trim() === '' || input_priority.value === null){
     return
   }
 
   todos.value.push({
-    content: input_content.value,
+    title: input_title.value,
+    content:input_content.value,
     priority: input_priority.value,
     done: false,
     createdAt: new Date().getTime()
   })
 
+  input_title.value = ''
   input_content.value = ''
   input_priority.value = null
 }
+
 const removeTodo = todo =>{
   todos.value = todos.value.filter(t => t !== todo)
 }
@@ -33,7 +37,6 @@ const removeTodo = todo =>{
 watch(todos, newVal => {
   localStorage.setItem('todos', JSON.stringify(newVal))
 }, { deep: true})
-
 
 
 watch(name, (newVal) => {
@@ -51,18 +54,50 @@ onMounted(() => {
 <template>
   <main class="app">
     <section class="greeting">
-      <h2 class="title">
+      <h1 class="title">
       Vítej v ToDoo <input type="text" placeholder="Tvoje jméno" v-model="name"/>
-      </h2>
+      </h1>
+      <h3 class="navAdd"><a id="navAdd" href="#task">Pridat ukol</a></h3>
+    </section>
+
+    
+    <!-- alt + 96 -->
+    <section class="todo-list">
+      <h3>Tvoje úkoly</h3>
+      <div class="list">
+          <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+            <label>
+              <input type="checkbox" v-model="todo.done"/>
+              <span :class="`bubble ${todo.priority}`"></span>
+            </label>
+            <div class="todo-content">
+              <input type="text" class="description" style="font-weight: 600;" v-model="todo.title"/>
+              <input type="text" class="description" v-model="todo.content"/>
+            </div>
+            <div class="actions">
+              <button class="delete" @click="removeTodo(todo)">Smazat</button>
+            </div>
+          </div>
+      </div>
+      <div v-if="todos.length == 0">
+        <h2 class="noTodos">Nemáš žádné úkoly. Měl bys nějaký přidat!</h2>
+      </div>
     </section>
 
     <section class="create-todo">
       <h3>Přidej další úkol</h3>
       <h4>Co bych tak chtěl dnes udělat?</h4>
       <form @submit.prevent="addTodo">
+
         <input 
         type="text" 
+        style="margin-bottom: 0.5rem;"
         placeholder="např. Vytvořit To Do aplikaci" 
+        v-model="input_title"/>
+        <h4>Přidej svému úkolu popis!</h4>
+        <input 
+        type="text"
+        placeholder="ukol = nazev, popis, priorita, drag&drop"
         v-model="input_content"/>
 
         <h3>Vyber prioritu úkolu</h3>
@@ -98,28 +133,9 @@ onMounted(() => {
 
         </div>
 
-        <input type="submit" value="Přidat úkol"/>
+        <input type="submit" value="Přidat úkol" id="task"/>
       </form>
     </section>
-    <!-- alt + 96 -->
-    <section class="todo-list">
-      <h3>Tvoje úkoly</h3>
-      <div class="list">
-          <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
-            <label>
-              <input type="checkbox" v-model="todo.done"/>
-              <span :class="`bubble ${todo.priority}`"></span>
-            </label>
-            <div class="todo-content">
-              <input type="text" v-model="todo.content"/>
-            </div>
-            <div class="actions">
-              <button class="delete" @click="removeTodo(todo)">Smazat</button>
-            </div>
-          </div>
-      </div>
-    </section>
-
   </main>
 
 </template>
